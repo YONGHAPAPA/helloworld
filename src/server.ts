49e763c8cc638648f16a97b5f6c8ab8f61138cd3
YONGHAPAPA <zerobreaker@gmail.com>
2020-02-20 17:15:36 +0900

    dev

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,11 @@ import * as express from 'express';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-console.log("this is server.ts");
+app.get('', (req, res) => { res.send('Hello World!'); });
+app.listen(PORT, () => {
+    console.log("this is server.ts");
+    console.log(`the server is listening from port ${PORT}`);
+})
+
+
 
